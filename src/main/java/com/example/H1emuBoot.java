8b9e6353f0377c4823004cbac62b505e8288d2d1
Dituon <2544193782@qq.com>
2022-08-31 20:08:44 +0800

    优化

--- src/main/java/com/example/H1emuBoot.java
+++ src/main/java/com/example/H1emuBoot.java
@@ -1,17 +1,24 @@
 package com.example;
 
-import net.mamoe.mirai.Mirai;
-import net.mamoe.mirai.console.extension.PluginComponentStorage;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
 import net.mamoe.mirai.event.GlobalEventChannel;
 import net.mamoe.mirai.event.Listener;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
 import net.mamoe.mirai.message.data.MessageSource;
-import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
 
 public final class H1emuBoot extends JavaPlugin {
     public static final H1emuBoot INSTANCE = new H1emuBoot();
+
+    public static final String[] recallKeyword = //关键词可以存数组
+            {
+                    "习近平",
+                    "李克强",
+                    "xxx"
+            };
 
     private H1emuBoot() {
         super(new JvmPluginDescriptionBuilder("com.example.h1emu-boot", "0.1.0")
@@ -20,28 +27,37 @@ public final class H1emuBoot extends JavaPlugin {
                 .build());
     }
 
-    @Override
-    public  void onLoad(@NotNull PluginComponentStorage $this$onLoad){
-
-    }
+    //监听onLoad无意义
+//    @Override
+//    public  void onLoad(@NotNull PluginComponentStorage $this$onLoad){
+//
+//    }
 
     @Override
     public void onEnable() {
         try {
             //监听群组消息
-            Listener listener = GlobalEventChannel.INSTANCE.subscribeAlways(GroupMessageEvent.class, event -> {
-                String content = event.getMessage().contentToString();
-                if (content.contains("习近平")) {
-                    MessageSource.recall(event.getSource());
-                }
-                event.getSubject().sendMessage("喵");
-            });
-        }catch (Exception e){
+            Listener<GroupMessageEvent> listener =
+                    GlobalEventChannel.INSTANCE.subscribeAlways(
+                            GroupMessageEvent.class,
+                            this::onGroupMessage //对应下面的方法
+                    );
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
 
-
+    private void onGroupMessage(GroupMessageEvent event) {
+        String content = event.getMessage().contentToString();
+//        if (content.contains("习近平")) {
+//            MessageSource.recall(event.getSource());
+//        }
+        for (String keyword : recallKeyword) {
+            if (content.contains(keyword)) {
+                MessageSource.recall(event.getSource());
+                event.getGroup().sendMessage("喵"); //推荐使用getGroup()
+                return;
+            }
+        }
+    }
 }
